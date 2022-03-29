a7776851c37d4e66bf9851394292ad813ec03ede
Administrator <Administrator@WIN-7GMTI8LU280>
2022-03-29 20:25:52 +0200

    Caso de estudio 5

--- src/main/java/services/ShoutService.java
+++ src/main/java/services/ShoutService.java
@@ -2,6 +2,8 @@
 package services;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -46,6 +48,22 @@ public class ShoutService {
 
 	public void save(final Shout shout) {
 		this.shoutRepository.save(shout);
+	}
+
+	public Map<String, Double> computeStatistics() {
+		Map<String, Double> result;
+		double countAll, countShort, countLong;
+
+		countAll = this.shoutRepository.countAllShouts();
+		countShort = this.shoutRepository.countShortShouts();
+		countLong = this.shoutRepository.countLongShouts();
+
+		result = new HashMap<String, Double>();
+		result.put("count.all.shouts", countAll);
+		result.put("count.short.shouts", countShort);
+		result.put("count.long.shouts", countLong);
+
+		return result;
 	}
 
 }
