f427aae870e873e6b7f92f166452bf6aebae8d6a
Administrator <Administrator@WIN-EQ7SMK6U6HB>
2024-04-26 14:12:49 +0200

    v0.1 proyecto acme-dance inicial

--- src/main/java/utilities/DatabaseConfig.java
+++ src/main/java/utilities/DatabaseConfig.java
@@ -12,7 +12,7 @@ package utilities;
 
 public interface DatabaseConfig {
 
-	public final String	PersistenceUnit				= "Acme-Shout";
+	public final String	PersistenceUnit				= "Acme-Dance";
 
 	public final String	entitySpecificationFilename	= "./src/main/resources/PopulateDatabase.xml";
 	public final String	entityMapFilename			= "./src/main/resources/Entities.map";
